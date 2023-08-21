a8111e2d2d0021b2cbc0f39bd99ef20bcfcb1a52
Dilu-M <88144478+Dilu-M@users.noreply.github.com>
2023-08-22 00:09:46 +0530

    Update mainfib.py

--- mainfib.py
+++ mainfib.py
@@ -1,11 +1,8 @@
 n = int(input("Enter the number of terms for the Fibonacci series: "))
-
 a, b = 0, 1
 fibonacci_series = []
-
 for _ in range(n):
     fibonacci_series.append(a)
     a, b = b, a + b
-
 print("Fibonacci Series:")
 print(" ".join(map(str, fibonacci_series)))
